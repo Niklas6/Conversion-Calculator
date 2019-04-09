91c1701480ef27b10e2a752cc61c3bd0a0270435
Knobel <nk29t@mu-019063.dhcp.missouri.edu>
2019-04-09 12:05:02 -0500

    progress

--- Conversion Calculator/ConverterViewController.swift
+++ Conversion Calculator/ConverterViewController.swift
@@ -16,7 +16,28 @@ class ConverterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func Converter(_ sender: UIButton) {let alert = UIAlertController(title: "", message: "Choose Converter", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "fahrenheit to celcius", style: UIAlertAction.Style.default, handler: {
+            (alertAction) -> Void in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "celcius to fahrenheit", style: UIAlertAction.Style.default, handler: {
+            (alertAction) -> Void in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "miles to kilometers", style: UIAlertAction.Style.default, handler: {
+            (alertAction) -> Void in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "kilometers to fahrenheit", style: UIAlertAction.Style.default, handler: {
+            (alertAction) -> Void in
+        }))
+        
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
