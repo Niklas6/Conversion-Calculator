649254848933059144c8a8a4b153e563d73d18b7
Knobel <nk29t@mu-019063.dhcp.missouri.edu>
2019-04-09 12:18:49 -0500

    progress

--- Conversion Calculator/ConverterViewController.swift
+++ Conversion Calculator/ConverterViewController.swift
@@ -10,27 +10,43 @@ import UIKit
 
 class ConverterViewController: UIViewController {
 
+    @IBOutlet weak var first: UITextField!
+    @IBOutlet weak var second: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.first.text = "°F"
+        self.second.text = "°C"
         // Do any additional setup after loading the view.
     }
     
     @IBAction func Converter(_ sender: UIButton) {let alert = UIAlertController(title: "", message: "Choose Converter", preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "fahrenheit to celcius", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.first.text = "°F"
+            self.second.text = "°C"
+            
         }))
         
         alert.addAction(UIAlertAction(title: "celcius to fahrenheit", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.first.text = "°C"
+            self.second.text = "°F"
+            
         }))
         
         alert.addAction(UIAlertAction(title: "miles to kilometers", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.first.text = "mi"
+            self.second.text = "km"
+            
         }))
         
         alert.addAction(UIAlertAction(title: "kilometers to fahrenheit", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.first.text = "km"
+            self.second.text = "mi"
         }))
         
         
